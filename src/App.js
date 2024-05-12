b357432aa30cb69520d04ebb1a31cd34e477619f
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2024-05-12 16:35:54 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Dajana and is{" "}
-        <a
-          href="https://github.com/dajana-lab/project-react-weather-app"
-          target="_blank"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        
+        <footer>
+          This project is coded by Dajana and is{" "}
+          <a
+            href="https://github.com/dajana-lab/project-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
