4a76f6fec23dd391c64a623560d5e5c058ab9625
dajana-lab <75635513+dajana-lab@users.noreply.github.com>
2024-05-12 16:07:52 +0200

    Added a GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Dajana and is{" "}
+        <a
+          href="https://github.com/dajana-lab/project-react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
